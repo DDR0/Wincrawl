48d80b24f3c01cfe96f47f08695434c77d5704dd
DDR <d@ddr0.ca>
2021-08-08 21:14:34 -0700

    Small refactor to triggers.cpp to remove spare casts.

--- Wincrawl2/triggers.cpp
+++ Wincrawl2/triggers.cpp
@@ -1,30 +1,28 @@
 #include <cstring>
 #include <iostream>
 
-
 #include "triggers.hpp"
 
-const bool Triggers::run(const char* cmd) {
-	const char* command = reinterpret_cast<const char*>(cmd); //Cast for str* functions - it's safe, and we only care about byte-level compatability in this case.
-	
+//Register a new callback.
+void Triggers::add(const char* seq, const std::function<void()> callback) {
+	this->triggers.emplace_back(seq, callback);
+}
+
+//Run a registered callback. Returns true if the command was a partial match for a trigger, and therefore wasn't consumed.
+const bool Triggers::run(const char* command) {
 	int commandMatchesTriggers{ false };
+
 	for (auto trigger : this->triggers) {
-		const char* triggerName = reinterpret_cast<const char*>(trigger.seq);
-		
 		//Command should be fully equal to run the cb - the full match is needed to absorb all of an escape sequence which could be unique by the second character.
-		if (!strcmp(triggerName, command)) {
+		if (!strcmp(trigger.seq, command)) {
 			std::cout << "\n"; //done input, advance to new line - this probably shouldn't be here, we should return the trigger that matched instead.
 			trigger.callback();
 			return false;
 		}
 		
-		//Otherwise, mark if our command was equal to part of a trigger. It could be completed in the future.
-		commandMatchesTriggers |=
-			!strncmp(triggerName, command, strlen(command));
+		//Mark if our command so far is a partial match to a trigger. It could be completed in the future as more characters are read in.
+		commandMatchesTriggers |= !strncmp(trigger.seq, command, strlen(command));
 	}
-	return commandMatchesTriggers;
-}
 
-void Triggers::add(const char* seq, const std::function<void()> callback) {
-	this->triggers.emplace_back(seq, callback);
+	return commandMatchesTriggers;
 }
